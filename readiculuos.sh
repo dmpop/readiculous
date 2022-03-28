55f596af9ac3d78416c6c32a88d350bd915854dd
Dmitri Popov <dmpop@linux.com>
2022-03-28 14:16:15 +0200

    Add auto mode

--- readiculuos.sh
+++ readiculuos.sh
@@ -13,18 +13,24 @@ $0 transforms web pages pages into readable EPUB files.
 
 USAGE:
 ------
-  $0 -u <URL> -d <dir>
+  $0 -u <URL> -d <dir> -m auto
 
 OPTIONS:
 --------
-  -u Specifies the source URL
-  -t Specifies a title of the output file
+  -u Source URL
   -d Destination directory
+  -m Enable auto mode
+
+EXAMPLES:
+---------
+$0 -u https://psyche.co/guides/how-to-approach-the-lifelong-project-of-language-learning -d "Language"
+$0 -m auto
+
 EOF
     exit 1
 }
 
-while getopts "u:d:" opt; do
+while getopts "u:d:m:" opt; do
     case ${opt} in
     u)
         url=$OPTARG
@@ -32,12 +38,44 @@ while getopts "u:d:" opt; do
     d)
         dir=$OPTARG
         ;;
+    m)
+        mode=$OPTARG
+        ;;
     \?)
         usage
         ;;
     esac
 done
 shift $((OPTIND - 1))
+
+r=$(shuf -i 0-255 -n 1)
+g=$(shuf -i 0-255 -n 1)
+b=$(shuf -i 0-255 -n 1)
+
+for f in fonts/*.ttf; do
+    embed_fonts+="--epub-embed-font="
+    embed_fonts+=$f
+    embed_fonts+=" "
+done
+
+if [ "$mode" = "auto" ]; then
+    file="links.txt"
+    if [ ! -f "$file" ]; then
+        echo "$file not found."
+        exit 1
+    fi
+    dir="Library"
+    mkdir -p "$dir"
+    while IFS="" read -r url || [ -n "$url" ]; do
+        title=$(curl -s $url | awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");print;exit}')
+        ./go-readability $url >>"$dir/$title".html
+        convert -size 1000x1000 xc:rgb\($r,$g,$b\) cover.png
+        convert -background '#0008' -font Open-Sans -pointsize 50 -fill white -gravity center -size 1000x150 caption:"$title" cover.png +swap -gravity center -composite cover.png
+        pandoc -f html -t epub --metadata title="$title" --metadata creator="Readiculous" --metadata publisher="$url" --css=stylesheet.css $embed_fonts --epub-cover-image=cover.png -o "$dir/$title".epub "$dir/$title".html
+        rm cover.png
+    done <"$file"
+    exit 1
+fi
 
 if [ -z "$url" ] || [ -z "$dir" ]; then
     usage
@@ -48,18 +86,8 @@ mkdir -p "$dir"
 title=$(curl -s $url | awk -vRS="</title>" '/<title>/{gsub(/.*<title>|\n+/,"");print;exit}')
 ./go-readability $url >>"$dir/$title".html
 
-r=$(shuf -i 0-255 -n 1)
-g=$(shuf -i 0-255 -n 1)
-b=$(shuf -i 0-255 -n 1)
-
 convert -size 1000x1000 xc:rgb\($r,$g,$b\) cover.png
 convert -background '#0008' -font Open-Sans -pointsize 50 -fill white -gravity center -size 1000x150 caption:"$title" cover.png +swap -gravity center -composite cover.png
-
-for f in fonts/*.ttf; do
-    embed_fonts+="--epub-embed-font="
-    embed_fonts+=$f
-    embed_fonts+=" "
-done
 
 pandoc -f html -t epub --metadata title="$title" --metadata creator="Readiculous" --metadata publisher="$url" --css=stylesheet.css $embed_fonts --epub-cover-image=cover.png -o "$dir/$title".epub "$dir/$title".html
 
